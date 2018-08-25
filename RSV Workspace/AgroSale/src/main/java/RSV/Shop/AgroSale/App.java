a012bb5ffdd533b9710a63370ec8d20cee7b7927
Niharika <Niharika@DESKTOP-GI2FRQJ>
2018-08-25 12:24:56 +0530

    My First Commit

---
+++ RSV Workspace/AgroSale/src/main/java/RSV/Shop/AgroSale/App.java
@@ -0,0 +1,13 @@
+package RSV.Shop.AgroSale;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
